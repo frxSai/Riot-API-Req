92114aa4f2164523b15c31306c6033355b50dcab
Aizend S. Grad <88302292+c0mrate@users.noreply.github.com>
2024-03-18 22:13:23 +0700

    .

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require __DIR__ . '/vendor/autoload.php';
 use Cowsayphp\Farm;
 
 // Redirect to src/login.php
-header('Location: /login.php');
+header('Location: src/login.php');
 exit();
 
 // This code will not execute after the redirect
