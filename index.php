5119840ee65b3a6300c790dc258331a0953a5fde
Aizend S. Grad <88302292+c0mrate@users.noreply.github.com>
2024-03-18 21:54:44 +0700

    .

--- index.php
+++ index.php
@@ -3,12 +3,8 @@ require __DIR__ . '/vendor/autoload.php';
 
 use Cowsayphp\Farm;
 
-header('Content-Type: text/plain');
+// Redirect to src/login.php
+header('Location: src/login.php');
+exit();
 
-$text = "/src/login.php";
-if(isset($_GET['message']) && $_GET['message'] != '') {
-	$text = htmlspecialchars($_GET['message']);
-}
-
-$cow = Farm::create(\Cowsayphp\Farm\Cow::class);
-echo $cow->say($text);
+// This code will not execute after the redirect
